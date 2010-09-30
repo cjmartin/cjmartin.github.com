abfa53b788889bdced58f1e92b1686c26b0d8a4c
cjmartin <cjmartin@gmail.com>
2010-09-30 11:40:22 -0700

    moved analytics js into body.js file

--- js/body.js
+++ js/body.js
@@ -9,3 +9,19 @@
 	}
 	document.write('<script charset="utf-8" type="text/javascript" src="http://disqus.com/forums/cjmartin/get_num_replies.js' + query + '"></' + 'script>');
 })();
+// End comment counts
+
+// Google Analytics
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+try {
+var pageTracker = _gat._getTracker("UA-7710190-4");
+pageTracker._trackPageview();
+} catch(err) {}
+// End Google Analytics
+
+// Woopra
+var _wh = ((document.location.protocol=='https:') ? "https://sec1.woopra.com" : "http://static.woopra.com");
+document.write(unescape("%3Cscript src='" + _wh + "/js/woopra.js' type='text/javascript'%3E%3C/script%3E"));
+// End Woopra
