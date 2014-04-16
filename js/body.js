ce1d63f446b9c6db30ab51a98914b1873895eb2c
Chris James Martin <cj@cjmart.in>
2014-04-16 11:40:47 -0700

    adding deleted example, removing resize js

--- js/body.js
+++ js/body.js
@@ -1,40 +1,40 @@
 // Flickr Web Embed
-(function() {
-	var frames = Array.prototype.slice.call(document.querySelectorAll("iframe[src*='//www.flickr.com/photos/']"));
-	var divisor;
-	var setHeight;
-	var resizeTimer;
+// (function() {
+// 	var frames = Array.prototype.slice.call(document.querySelectorAll("iframe[src*='//www.flickr.com/photos/']"));
+// 	var divisor;
+// 	var setHeight;
+// 	var resizeTimer;
 
-	function resizeFrames(event){
-		frames.forEach(function(frame, index, array){			
-			if (getComputedStyle(frame,null).getPropertyValue("max-width") == "none") {
-				frame.style.maxWidth = "100%";
-			}
+// 	function resizeFrames(event){
+// 		frames.forEach(function(frame, index, array){			
+// 			if (getComputedStyle(frame,null).getPropertyValue("max-width") == "none") {
+// 				frame.style.maxWidth = "100%";
+// 			}
 
-			// Set initial lastWidth if it's not set and offsetWidth is frame.width
-			if (!frame.dataset.lastWidth && frame.offsetWidth == frame.width) {
-				frame.dataset.lastWidth = frame.offsetWidth;
-			}
+// 			// Set initial lastWidth if it's not set and offsetWidth is frame.width
+// 			if (!frame.dataset.lastWidth && frame.offsetWidth == frame.width) {
+// 				frame.dataset.lastWidth = frame.offsetWidth;
+// 			}
 
-			if (frame.dataset.lastWidth != frame.offsetWidth && frame.offsetWidth <= frame.width) {
-				divisor = frame.width/frame.offsetWidth;
-				setHeight = frame.offsetHeight/divisor;
+// 			if (frame.dataset.lastWidth != frame.offsetWidth && frame.offsetWidth <= frame.width) {
+// 				divisor = frame.width/frame.offsetWidth;
+// 				setHeight = frame.offsetHeight/divisor;
 
-				frame.style.height = setHeight + "px";
-				frame.dataset.lastWidth = frame.offsetWidth;
-			}
-		});
-	}
+// 				frame.style.height = setHeight + "px";
+// 				frame.dataset.lastWidth = frame.offsetWidth;
+// 			}
+// 		});
+// 	}
 
-	if (frames) {
-		window.addEventListener('resize', function(){
-			clearTimeout(resizeTimer);
-			resizeTimer = setTimeout(resizeFrames, 250);
-		}, false);
+// 	if (frames) {
+// 		window.addEventListener('resize', function(){
+// 			clearTimeout(resizeTimer);
+// 			resizeTimer = setTimeout(resizeFrames, 250);
+// 		}, false);
 
-		resizeFrames();
-	}
-})();
+// 		resizeFrames();
+// 	}
+// })();
 
 // Disqus comment counts
 (function() {
