ff30033f6ce4e58463a5093a34ed23ecee085cd0
Chris Martin <cjmartin@gmail.com>
2025-04-09 13:31:33 -0700

    remove logging from permalink generator

--- _plugins/add_custom_permalink.rb
+++ _plugins/add_custom_permalink.rb
@@ -2,23 +2,15 @@
 require 'time'
 
 Jekyll::Hooks.register :site, :post_read do |site|
-  Jekyll.logger.info "Custom Permalink Plugin:", "Starting to process posts..."
-
   site.posts.docs.each do |post|
-    # Log the post being processed
-    Jekyll.logger.info "Custom Permalink Plugin:", "Processing post: #{post.path}"
-
     # Determine the link_category
     link_category = post.data['link_category']
-    if link_category
-      Jekyll.logger.info "Custom Permalink Plugin:", "Found link_category: #{link_category}"
-    elsif post.data['categories'] && post.data['categories'].any?
+    if !link_category && post.data['categories'] && post.data['categories'].any?
       link_category = post.data['categories'].first
-      Jekyll.logger.info "Custom Permalink Plugin:", "Using first category from categories: #{link_category}"
-    else
-      Jekyll.logger.info "Custom Permalink Plugin:", "No link_category or categories found for post: #{post.path}"
-      next # Skip this post if no link_category or categories are available
     end
+
+    # Skip this post if no link_category or categories are available
+    next unless link_category
 
     # Extract year and month from the post date
     year = post.date.strftime('%Y')
@@ -27,11 +19,8 @@ Jekyll::Hooks.register :site, :post_read do |site|
 
     # Generate the custom permalink
     custom_permalink = "/#{link_category}/#{year}/#{month}/#{slug}/"
-    Jekyll.logger.info "Custom Permalink Plugin:", "Generated custom permalink: #{custom_permalink}"
 
     # Set the custom permalink
     post.data['permalink'] = custom_permalink
   end
-
-  Jekyll.logger.info "Custom Permalink Plugin:", "Finished processing posts."
 end
